f74e4640d0fc29be01c1d270f7afa693b90317ef
mohamed <mohamedzaatouti@gmail.com>
2021-12-27 14:51:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import './style.css';
+import imageInSrc from './imageInSrc.jpg';
+//import Video from './myVideo.mp4';
+function App() {
+  return (
+    <div className="App">
+    <div className="checkpoint" style={{border:"solid 1px black",maxWidth:"100vw", }}>
+      <h1 className={"title red"}>Mohamed Zaatouti</h1>
+      <br/>
+      <img src={imageInSrc} alt='image in the src folder'/>
+      <br/>
+      <img src="/imageInPublic.jpg" alt='image in the public folder'/>
+      <br/>
+      <video width="720" height="500" controls>
+      {/* <source src={Video} type="video/mp4"/> */}
+      </video>
+      </div>
+    </div>  
+  );
+};
+
+
+export default App;
